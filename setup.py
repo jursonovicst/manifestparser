f1e12766a31876146bafee9e19a53170ebe6090f
jursonovicst <github.com@jursonovics.com>
2019-03-07 12:35:19 +0100

    first try

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='manifestparser',
+    version='v0.1',
+    packages=['', 'manifestparser'],
+    url='',
+    license='',
+    author='jursonovicst',
+    author_email='',
+    description=''
+)
